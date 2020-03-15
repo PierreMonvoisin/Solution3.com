6121a53901e425607eda93547a07cf9d64498567
Monvoisin <monvoisin.pierre@gmail.com>
2020-03-15 01:22:47 +0100

    dnf functionnal, different penalty for different solve bug fixed

--- assets/js/timer/solveDetails.js
+++ assets/js/timer/solveDetails.js
@@ -1,13 +1,40 @@
 $(function(){
+  var plusTwoPenalty = false;
+  var dnfPenalty = false;
+  var solveIndex = 0;
   $('#add2').click(function(){
-    var modalTime = $('#solveChosen').val();
-    var solveIndex = $('#solveId span').html();
-    var historyTime = $('#' + solveIndex + ' .timeValue').html();
-    var testTime = addTwoSeconds(modalTime);
-    console.log(testTime);
-    var sideStatsIndex = $('#sideStatIndex').html();
-    if (solveIndex == sideStatsIndex){
-      // Change time
+    alert(solveIndex);
+    var currentSolveIndex = $('#solveId span').html();
+    if (currentSolveIndex != solveIndex && $('#solveChosen').val() != 'DNF'){
+      plusTwoPenalty = false; dnfPenalty = false;
+      if (plusTwoPenalty != true && dnfPenalty != true){
+        var modalTime = $('#solveChosen').val();
+        var timePlusTwo = addTwoSeconds(modalTime);
+        $('#solveChosen').val(timePlusTwo);
+        $('#' + currentSolveIndex + ' .timeValue').html(timePlusTwo);
+        var sideStatsIndex = $('#sideStatIndex').html();
+        if (currentSolveIndex == sideStatsIndex){
+          $('#sideStatIndex').html(timePlusTwo);
+        }
+        solveIndex = currentSolveIndex;
+      }
+      plusTwoPenalty = true;
+    }
+  })
+  $('#dnf').click(function(){
+    var currentSolveIndex = $('#solveId span').html();
+    if (currentSolveIndex != solveIndex && $('#solveChosen').val() != 'DNF'){
+      plusTwoPenalty = false; dnfPenalty = false;
+      if (dnfPenalty != true){
+        $('#solveChosen').val('DNF');
+        $('#' + currentSolveIndex + ' .timeValue').html('DNF');
+        var sideStatsIndex = $('#sideStatIndex').html();
+        if (currentSolveIndex == sideStatsIndex){
+          $('#sideStatIndex').html('DNF');
+        }
+        solveIndex = currentSolveIndex;
+      }
+      dnfPenalty = true;
     }
   })
 });
