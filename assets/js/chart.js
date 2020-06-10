b533483f0dd42470fea01b275526f7ee712c1eba
Monvoisin <monvoisin.pierre@gmail.com>
2020-06-10 22:41:14 +0200

    dynamic data lines in chart

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,97 +1,96 @@
-  var chart = new CanvasJS.Chart('chartContainer', {
-    interactivityEnabled: false,
-    theme: 'light2',
-    animationEnabled: true,
-    animationDuration: 1250,
-    legend: {
-      fontSize: 13,
-      verticalAlign: 'top'
-    },
-    exportFileName: "Graphique des résolutions et moyennes",
-		exportEnabled: true,
-    axisX: {
-      lineThickness: 0,
-      labelFontSize: 0
-    },
-    axisY: {
-      includeZero: false,
-      lineColor: 'black',
-      gridThickness: 0.5,
-      gridColor: 'black',
-      labelFontColor: 'black',
-      labelFontSize: 13,
-      labelFormatter: function(e){
-        var timeFormatted = millisecondsToFullTime(e.value);
-        timeFormatted = (timeFormatted.split('.'))[0];
-        return timeFormatted;
+var dataSingle = [];
+var dataAo5 = [];
+var dataAo12 = [];
+// If the local storage is available
+if (typeof(Storage) != "undefined") {
+  // If there is at least one solve in the local storage
+  if (localStorage.getItem('index')){
+    var numberOfSolve = Number(localStorage.getItem('index'));
+    // As long as the solve found is null, get the previous solve
+    while (localStorage.getItem('index_' + numberOfSolve) == null  && numberOfSolve > 0){
+      numberOfSolve--;
+    }
+    if (numberOfSolve > 0){
+      // For each solve
+      for (numberOfSolve; numberOfSolve >= 1; numberOfSolve--){
+        // Get all solves and averages value
+        var singleTime = Number(localStorage.getItem(`time_${numberOfSolve}`));
+        var ao5Time = Number(localStorage.getItem(`ao5_${numberOfSolve}`));
+        var ao12Time = Number(localStorage.getItem(`ao12_${numberOfSolve}`));
+        if ( isNaN(singleTime) || singleTime == 0){ singleTime = null; }
+        if ( isNaN(ao5Time) || ao5Time == 0){ ao5Time = null; }
+        if ( isNaN(ao12Time) || ao12Time == 0){ ao12Time = null; }
+        // Format values
+        singleTime = {y: singleTime};
+        ao5Time = {y: ao5Time};
+        ao12Time = {y: ao12Time};
+        // Put all values in array for the chart
+        dataSingle.push(singleTime);
+        dataAo5.push(ao5Time);
+        dataAo12.push(ao12Time);
       }
-    },
-    data: [{
-      name: 'résolutions',
-      type: 'line',
-      color: 'grey',
-      xValueFormatString:'N° ########',
-      showInLegend: true,
-      markerType: 'none',
-      dataPoints: [
-        {y: 16524},
-        {y: 16093},
-        {y: 15056},
-        {y: 15062},
-        {y: 19897},
-        {y: 25205},
-        {y: 19019},
-        {y: 17192},
-        {y: 20119},
-        {y: 17400},
-        {y: 20042},
-        {y: 15306}
-      ]
-    },
-    {
-      name: 'moyennes de 5',
-      type: 'line',
-      color: 'blue',
-      xValueFormatString:'N° ########',
-      showInLegend: true,
-      markerType: 'none',
-      dataPoints: [
-        {y: 15893},
-        {y: 17017},
-        {y: 17993},
-        {y: 18703},
-        {y: 19678},
-        {y: 18846},
-        {y: 18820},
-        {y: 18211},
-        {y: 17583},
-        {y: 16282},
-        {y: 16031},
-        {y: 16009}
-      ]
-    },
-    {
-      name: 'moyennes de 12',
-      type: 'line',
-      color: 'red',
-      xValueFormatString:'N° ########',
-      showInLegend: true,
-      markerType: 'none',
-      dataPoints: [
-        {y: 16059},
-        {y: 15927},
-        {y: 15231},
-        {y: 16076},
-        {y: 16213},
-        {y: 16213},
-        {y: 16067},
-        {y: 16307},
-        {y: 16056},
-        {y: 15697},
-        {y: 15634},
-        {y: 15582}
-      ]
-    }]
-  });
-  // Render chart
-  chart.render();
+    }
+  }
+} else {
+  console.warn('Impossible d\'afficher les résolutions depuis le stockage local du navigateur !');
+}
+// Create the chart
+var chart = new CanvasJS.Chart('chartContainer', {
+  interactivityEnabled: false,
+  theme: 'light2',
+  animationEnabled: true,
+  animationDuration: 1250,
+  legend: {
+    fontSize: 13,
+    verticalAlign: 'top'
+  },
+  exportFileName: 'Graphique des résolutions et moyennes',
+  exportEnabled: true,
+  axisX: {
+    reversed:  true,
+    lineThickness: 0,
+    labelFontSize: 0
+  },
+  axisY: {
+    includeZero: false,
+    lineColor: 'black',
+    gridThickness: 0.5,
+    gridColor: 'black',
+    labelFontColor: 'black',
+    labelFontSize: 13,
+    labelFormatter: function(e){
+      var timeFormatted = millisecondsToFullTime(e.value);
+      timeFormatted = (timeFormatted.split('.'))[0];
+      return timeFormatted.replace(/\s+/g, ' ').trim();
+    }
+  },
+  data: [{
+    name: 'Résolutions',
+    type: 'line',
+    color: 'grey',
+    xValueFormatString:'N° ########',
+    showInLegend: true,
+    markerType: 'none',
+    dataPoints: dataSingle
+  },
+  {
+    name: 'Moyennes de 5',
+    type: 'line',
+    color: 'blue',
+    xValueFormatString:'N° ########',
+    showInLegend: true,
+    markerType: 'none',
+    dataPoints: dataAo5
+  },
+  {
+    name: 'Moyennes de 12',
+    type: 'line',
+    color: 'red',
+    xValueFormatString:'N° ########',
+    showInLegend: true,
+    markerType: 'none',
+    dataPoints: dataAo12
+  }]
+});
+// Render chart
+chart.render();
